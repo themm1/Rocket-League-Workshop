ffe7b94cff23971f00738a47554753b3d0852c20
themm1 <madzin.m@gmail.com>
2021-03-13 22:30:50 +0100

    popular maps table added

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from os import listdir
 from tkinter import *
 from main import loadMap, downloadMap
@@ -6,23 +7,23 @@ from main import loadMap, downloadMap
 
 class MyApp:
     def __init__(self):
-        self.root = Tk(className="My Window")
+        self.root = Tk(className="Rocket League Workshop")
         self.root.geometry("700x500")
         self.initUI()
         self.root.mainloop()
 
     def initUI(self):
-        frame = self.makeTable()
+        frame = self.downloadMapsTable()
         text_input = Entry(self.root, width=30, borderwidth=5)
         loadMapButton = Button(self.root, text="Load Map", padx=20)
         downloadButton = Button(self.root, text="Download Map", padx=20)
 
-        frame.pack(padx=10, pady=30)
-        text_input.place(anchor="nw")
-        loadMapButton.place(relx=0.8, y=0, anchor="ne")
-        downloadButton.place(relx=1, y=0, anchor="ne")
+        frame.pack(padx=10, pady=35)
+        text_input.place(relx=0.01, rely=0.01, anchor="nw")
+        loadMapButton.place(relx=0.8, rely=0.01, anchor="ne")
+        downloadButton.place(relx=0.99, rely=0.01, anchor="ne")
 
-    def makeTable(self):
+    def loadMapsTable(self):
         self.RL_PATH = r'E:\Rocket\rocketleague\TAGame\CookedPCConsole'
         maps = self.listFiles("./Map Files/")
         frame = LabelFrame(self.root, text="Maps", padx=5, pady=5)
@@ -39,6 +40,23 @@ class MyApp:
 
         return frame
 
+    def downloadMapsTable(self):
+        maps = self.listCSV("maps.csv")
+        frame = LabelFrame(self.root, text="Popular Worshop Maps", padx=5, pady=5)
+
+        buttons = []
+        for i in range(len(maps)):
+            for j in range(len(maps[i])):
+                frame.columnconfigure(j, minsize=100)
+                l = Label(frame, text=maps[i][j])
+                l.grid(row=i, column=j)
+        
+            buttons.append(Button(frame, text="Download", padx=10,
+                command=lambda map_id=maps[i][2]:downloadMap(map_id)))
+            buttons[i].grid(row=i, column=3)
+
+        return frame
+
     @staticmethod
     def listFiles(directory):
         maps = []
@@ -47,6 +65,16 @@ class MyApp:
                 size = os.path.getsize(f"{directory}{file}")
                 maps.append([file, f"{round(size / 1000000, 2)} MB"])
         return maps
+
+    @staticmethod
+    def listCSV(file):
+        maps = []
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                maps.append(row)
+        return maps
+
 
 if __name__ == "__main__":
     MyApp()
